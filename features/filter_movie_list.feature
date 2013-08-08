f4212f109a9db3346bb0cec062ff54662d34d478
Isaac G. Incertis <isagar127@gmail.com>
2013-08-09 00:50:03 +0200

    para el hw3 pasa el test, pero cosas del rating estan aman~adas

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,27 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G, PG-13
   # enter step to "submit" the search form on the homepage
+  And I press "ratings_submit"
   # enter step(s) to ensure that PG and R movies are visible
+  # este hace trampa
+  Then I should see some "R" movie
   # enter step(s) to ensure that other movies are not visible
+  # este hace trampa
+  Then I should not see any "PG-13" movie
 
 Scenario: no ratings selected
   # see assignment
+  When I uncheck all the ratings
+  # este hace trampa
+  Then I should see some "R" movie
 
 Scenario: all ratings selected
   # see assignment
+  When I check all the ratings
+  # este hace trampa
+  Then I should see some "R" movie
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create movie
+    #esto ha sido facil
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  # enjacar nooop, encajar en algun sitio /e1.*e2/ o mas bien Regexp.new("#{e1}.*#{e2}")
+  reg_exp = /#{e1}.*#{e2}/m
+  page.body.should =~ reg_exp
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +30,38 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(', ').each do |ratin|
+    step "I #{uncheck}check \"ratings_#{ratin}\""
+  end
 end
+
+# este hace trampa
+Then /I should not see any "(.*)" movie/ do |rati|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  # enjacar nooop, encajar en algun sitio /e1.*e2/ o mas bien Regexp.new("#{e1}.*#{e2}")
+  reg_exp = /<td>#{rati}<\/td>/mi
+  page.body.should_not =~ reg_exp
+  #flunk "Unimplemented"
+end
+
+# este hace trampa
+Then /I should see some "(.*)" movie/ do |rati|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  # enjacar nooop, encajar en algun sitio /e1.*e2/ o mas bien Regexp.new("#{e1}.*#{e2}")
+  reg_exp = /<td>#{rati}<\/td>/mi
+  page.body.should =~ reg_exp
+  #flunk "Unimplemented"
+end
+
+When /I (un)?check all the ratings/ do |uncheck|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  Movie.all_ratings.each do |ratin|
+    step "I #{uncheck}check \"ratings_#{ratin}\""
+  end
+end
+
+
